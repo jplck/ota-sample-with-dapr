efdc5535f56eafdc0eb1bdd035734cd9872b9ae5
jplck <pollack.jan@gmail.com>
2021-03-24 16:17:36 +0100

    added telemetry-sender

---
+++ src/telemetry-sender/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Dapr.Client;
+using System.Threading.Tasks;
+
+namespace telemetry_sender
+{
+    class Program
+    {
+        private const int _numberOfRuns = 1000;
+
+        private const int _delay = 1000;
+        private const string _ehBindingName = "telemetry-binding";
+
+        static async Task Main(string[] args)
+        {
+            var daprClient = new DaprClientBuilder().Build();
+            var idx = 0;
+            while (idx <= _numberOfRuns)
+            {
+                await daprClient.InvokeBindingAsync(_ehBindingName, "create", idx);
+                idx++;
+                Console.WriteLine(idx);
+                await Task.Delay(_delay);
+            }
+        }
+    }
+}
