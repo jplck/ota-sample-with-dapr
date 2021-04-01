a77a04d7b1a1b284ec5ff42510853fd32f55abe3
jplck <pollack.jan@gmail.com>
2021-04-01 11:23:52 +0200

    update device telemetry sender goroutines

--- src/device-telemetry-sender/go.mod
+++ src/device-telemetry-sender/go.mod
@@ -2,4 +2,7 @@ module github.com/jplck/ota-sample-with-dapr/tree/main/src/device-telemetry-send
 
 go 1.16
 
-require github.com/dapr/go-sdk v1.0.0 // indirect
+require (
+	github.com/dapr/go-sdk v1.0.0 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+)
--- src/device-telemetry-sender/src/main.go
+++ src/device-telemetry-sender/src/main.go
@@ -5,23 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	dapr "github.com/dapr/go-sdk/client"
 	"github.com/dapr/go-sdk/service/common"
 	daprd "github.com/dapr/go-sdk/service/http"
+	"github.com/google/uuid"
 )
 
-var (
-	sub = &common.Subscription{
-		PubsubName: "credentialrequest-pubsub",
-		Topic:      "credentialrequests",
-		Route:      "/command",
-	}
-
-	simActive = true
-	modeChan  chan bool
-)
+var sub = &common.Subscription{
+	PubsubName: "credentialrequest-pubsub",
+	Topic:      "credentialrequests",
+	Route:      "/command",
+}
 
 const (
 	pubTopic            = "telemetry"
@@ -29,27 +28,56 @@ const (
 	port                = 8000
 )
 
-func init() {
-	modeChan = make(chan bool)
-}
-
 func main() {
 
-	defer close(modeChan)
-	go publishTelemetry()
+	signalChan := make(chan bool)
+	simStatus := true
+	e := make(chan os.Signal, 1)
+	signal.Notify(e, os.Interrupt, syscall.SIGTERM)
 
-	s := daprd.NewService(fmt.Sprintf(":%d", port))
+	/*
+		Run simulation always on startup. The signal channel is used to
+		stop the simulation if the associated command comes in from
+		remote
+	*/
+	go runSim(signalChan)
 
-	if err := s.AddTopicEventHandler(sub, commandEventHandler); err != nil {
-		log.Fatalf("error adding topic subscription: %v", err)
-	}
+	/*
+		Run dapr service in a goroutine. This is required to be able to
+		synchronize the channels between the two components (sim, dapr)
+	*/
+	go func(signalChan chan bool) {
+		s := daprd.NewService(fmt.Sprintf(":%d", port))
 
-	if err := s.Start(); err != nil && err != http.ErrServerClosed {
-		log.Fatalf("error listenning: %v", err)
-	}
+		//Receive commands and signal the currently running goroutine if stop is required
+		commandHandler := func(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
+			command := parseCommand(e, signalChan)
+			if !command {
+				//STOP
+				signalChan <- false
+				simStatus = false
+			} else {
+				//START NEW SIM
+				if !simStatus {
+					go runSim(signalChan)
+				}
+			}
+			return false, nil
+		}
+
+		if err := s.AddTopicEventHandler(sub, commandHandler); err != nil {
+			log.Fatalf("error adding topic subscription: %v", err)
+		}
+
+		if err := s.Start(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("error listenning: %v", err)
+		}
+	}(signalChan)
+
+	<-e
 }
 
-func publishTelemetry() {
+func runSim(stopSignal chan bool) {
 	log.Println("Start publishing telemetry.")
 
 	daprClient, err := dapr.NewClient()
@@ -59,44 +87,36 @@ func publishTelemetry() {
 
 	defer daprClient.Close()
 
-	go func() {
-		ctx := context.Background()
-		i := 0
-		for {
+	ctx := context.Background()
 
-			if !simActive {
-				log.Println("Exiting sim loop.")
-				break
-			}
-
-			err := daprClient.PublishEvent(
-				ctx,
-				telemetryPubSubName,
-				pubTopic,
-				[]byte(fmt.Sprintf("telemetry payload: %d", i)))
-
-			if err != nil {
-				log.Printf("Unable to publish: %v", err.Error())
-			}
-
+	for {
+		select {
+		case <-stopSignal:
+			break
+		default:
+			log.Println("Publishing telemetry")
+			pubTelemetry(daprClient, ctx)
 			time.Sleep(time.Second)
-			i++
 		}
-	}()
-
-	simActive = <-modeChan
+	}
 }
 
-func commandEventHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
+func pubTelemetry(daprClient dapr.Client, ctx context.Context) {
+	err := daprClient.PublishEvent(
+		ctx,
+		telemetryPubSubName,
+		pubTopic,
+		[]byte(fmt.Sprintf("telemetry payload: %s", uuid.New().String())))
+
+	if err != nil {
+		log.Printf("Unable to publish: %v", err.Error())
+	}
+}
+
+func parseCommand(e *common.TopicEvent, signal chan bool) bool {
 	log.Printf("event - PubsubName: %s, Topic: %s, ID: %s, Data: %s", e.PubsubName, e.Topic, e.ID, e.Data)
 
 	mode := false
 
-	if mode && !simActive {
-		publishTelemetry()
-	} else if !mode && simActive {
-		simActive = false
-	}
-
-	return false, nil
+	return mode
 }
