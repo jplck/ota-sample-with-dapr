05bd7437d7e9b51201c765c3f2abb0359005006a
jplck <pollack.jan@gmail.com>
2021-04-05 12:34:07 +0200

    refactoring

--- src/device-telemetry-sender/src/main.go
+++ src/device-telemetry-sender/src/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	dapr "github.com/dapr/go-sdk/client"
@@ -16,11 +14,16 @@ import (
 	"github.com/google/uuid"
 )
 
-var sub = &common.Subscription{
-	PubsubName: "credentialrequest-pubsub",
-	Topic:      "credentialrequests",
-	Route:      "/command",
-}
+var (
+	sub = &common.Subscription{
+		PubsubName: "credentialrequest-pubsub",
+		Topic:      "credentialrequests",
+		Route:      "/command",
+	}
+	simStatus  = false
+	sig        chan bool
+	daprClient dapr.Client
+)
 
 const (
 	pubTopic            = "telemetry"
@@ -28,80 +31,96 @@ const (
 	port                = 8000
 )
 
-func main() {
-
-	signalChan := make(chan bool)
-	simStatus := true
-	e := make(chan os.Signal, 1)
-	signal.Notify(e, os.Interrupt, syscall.SIGTERM)
-
-	/*
-		Run simulation always on startup. The signal channel is used to
-		stop the simulation if the associated command comes in from
-		remote
-	*/
-	go runSim(signalChan)
-
-	/*
-		Run dapr service in a goroutine. This is required to be able to
-		synchronize the channels between the two components (sim, dapr)
-	*/
-	go func(signalChan chan bool) {
-		s := daprd.NewService(fmt.Sprintf(":%d", port))
-
-		//Receive commands and signal the currently running goroutine if stop is required
-		commandHandler := func(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
-			command := parseCommand(e, signalChan)
-			if !command {
-				//STOP
-				signalChan <- false
-				simStatus = false
-			} else {
-				//START NEW SIM
-				if !simStatus {
-					go runSim(signalChan)
-				}
-			}
-			return false, nil
-		}
-
-		if err := s.AddTopicEventHandler(sub, commandHandler); err != nil {
-			log.Fatalf("error adding topic subscription: %v", err)
-		}
-
-		if err := s.Start(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("error listenning: %v", err)
-		}
-	}(signalChan)
-
-	<-e
+func init() {
+	c, err := dapr.NewClient()
+	if err != nil {
+		log.Fatalf("Unable to create new dapr client due to err: %v", err.Error())
+	}
+	daprClient = c
 }
 
-func runSim(stopSignal chan bool) {
-	log.Println("Start publishing telemetry.")
-
-	daprClient, err := dapr.NewClient()
-	if err != nil {
-		log.Fatal("Unable to setup dapr client.")
-	}
+func main() {
 
 	defer daprClient.Close()
 
+	go func() {
+
+		time.Sleep(time.Second * 10)
+
+		triggerCmd("start")
+
+		time.Sleep(time.Second * 5)
+
+		triggerCmd("stop")
+
+		time.Sleep(time.Second * 5)
+
+		triggerCmd("start")
+	}()
+
+	s := daprd.NewService(fmt.Sprintf(":%d", port))
+
+	if err := s.AddTopicEventHandler(sub, commandHandler); err != nil {
+		log.Fatalf("error adding topic subscription: %v", err)
+	}
+
+	if err := s.Start(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("error listenning: %v", err)
+	}
+}
+
+func commandHandler(ctx context.Context, e *common.TopicEvent) (retry bool, err error) {
+	var cmdReq CommandRequest
+	if err := json.Unmarshal([]byte(fmt.Sprint(e.Data)), &cmdReq); err != nil {
+		log.Fatalf("Unable to parse incoming command due to error: %v", err.Error())
+	}
+
+	if cmdReq.Command == "stop" {
+		//STOP
+		close(sig)
+		simStatus = false
+	} else {
+		//START NEW SIM
+		if !simStatus {
+			sig = make(chan bool)
+			go runSim(sig)
+		}
+	}
+	return false, nil
+}
+
+func triggerCmd(cmd string) {
+	cmdReq := CommandRequest{
+		Command: cmd,
+	}
+
+	json, _ := json.Marshal(cmdReq)
+
 	ctx := context.Background()
+
+	if err := daprClient.PublishEvent(ctx, sub.PubsubName, sub.Topic, json); err != nil {
+		log.Printf("Unable to trigger sim from self due to error: %v", err.Error())
+	}
+}
+
+func runSim(sig chan bool) {
+	log.Println("Start publishing telemetry.")
 
 	for {
 		select {
-		case <-stopSignal:
-			break
+		case <-sig:
+			log.Println("Stopping simulation.")
+			return
 		default:
 			log.Println("Publishing telemetry")
-			pubTelemetry(daprClient, ctx)
+			pubTelemetry(daprClient)
 			time.Sleep(time.Second)
 		}
 	}
 }
 
-func pubTelemetry(daprClient dapr.Client, ctx context.Context) {
+func pubTelemetry(daprClient dapr.Client) {
+	ctx := context.Background()
 	err := daprClient.PublishEvent(
 		ctx,
 		telemetryPubSubName,
@@ -111,12 +130,4 @@ func pubTelemetry(daprClient dapr.Client, ctx context.Context) {
 	if err != nil {
 		log.Printf("Unable to publish: %v", err.Error())
 	}
-}
-
-func parseCommand(e *common.TopicEvent, signal chan bool) bool {
-	log.Printf("event - PubsubName: %s, Topic: %s, ID: %s, Data: %s", e.PubsubName, e.Topic, e.ID, e.Data)
-
-	mode := false
-
-	return mode
 }
--- src/device-telemetry-sender/src/model.go
+++ src/device-telemetry-sender/src/model.go
@@ -1,5 +1,16 @@
 package main
 
-type Command struct {
-	
+import "time"
+
+type CommandRequest struct {
+	Command string `json:"command"`
+}
+
+type CloudEvent struct {
+	ID          string      `json:"id"`
+	Source      string      `json:"source"`
+	SpecVersion string      `json:"specversion"`
+	Type        string      `json:"type"`
+	Time        time.Time   `json:"time"`
+	Data        interface{} `json:"data"`
 }
